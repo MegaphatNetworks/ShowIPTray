36caaeb9aff74040663c6c888991da361709ec6b
Megaphat Networks <60324301+MegaphatNetworks@users.noreply.github.com>
2021-11-07 16:57:51 -0500

    Create ShowIPTray.ps1

---
+++ ShowIPTray.ps1
@@ -0,0 +1,187 @@
+function ico0
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,12)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,12)
+	}
+
+function ico1
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,4,1+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,13,1+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,3,2+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,12,4+$off,13)
+	}
+
+function ico2
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,9,1+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,8,2+$off,9)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,7,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,7)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,11,4+$off,13)
+	}
+
+function ico3
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,11,1+$off,12)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,7,2+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,7,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,6)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,9,4+$off,12)
+	}
+
+function ico4
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,7,1+$off,10)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,5,2+$off,6)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,9,2+$off,10)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,3,3+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,9,3+$off,10)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,2,4+$off,13)
+	}	
+	
+function ico5
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,2,1+$off,7)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,12,1+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,6,2+$off,7)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,6,3+$off,7)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,2,4+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,7,4+$off,12)
+	}	
+	
+function ico6
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,12)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,7,2+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,7,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,4)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,8,4+$off,12)
+	}	
+	
+function ico7
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,2,1+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,10,1+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,8,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,6,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,2,4+$off,7)
+	}	
+	
+function ico8
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,6)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,9,1+$off,12)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,7,2+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,7,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,6)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,9,4+$off,12)
+	}	
+	
+function ico9
+	{
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,3,1+$off,7)
+		$g.drawline([System.Drawing.Pens]::Yellow,1+$off,11,1+$off,12)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,2,2+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,7,2+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,2+$off,12,2+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,2,3+$off,3)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,7,3+$off,8)
+		$g.drawline([System.Drawing.Pens]::Yellow,3+$off,12,3+$off,13)
+		$g.drawline([System.Drawing.Pens]::Yellow,4+$off,3,4+$off,12)
+	}
+
+	[Reflection.Assembly]::LoadWithPartialName("System.Drawing") | Out-Null
+	$bmp = New-Object System.Drawing.Bitmap(16,16)
+	$g = [System.Drawing.Graphics]::FromImage($bmp)
+
+	# find ipaddress
+	$IPAddress = ((ipconfig | findstr [0-9].\.)[0]).Split()[-1]
+
+	$NodeIP = $IPAddress.split(".")[3]
+	$NodeIPLen = ($NodeIP | measure-object -character | select -expandproperty characters)
+	If ($NodeIPLen -eq 2) {$NodeIP = "0" + $NodeIP}
+	If ($NodeIPLen -eq 1) {$NodeIP = "00" + $NodeIP}
+		
+	$NodeIP1 = $NodeIP.substring(0,1)
+	$NodeIP2 = $NodeIP.substring(1,1)
+	$NodeIP3 = $NodeIP.substring(2,1)
+
+	$i = 0
+	do
+		{
+			if ([string]$i -eq $NodeIP1) {$function1 = "ico$i"}
+			$i++
+		}  
+	while ($i -le 9)
+
+	$i = 0
+	do
+		{
+			if ([string]$i -eq $NodeIP2) {$function2 = "ico$i"}
+			$i++
+		}  
+	while ($i -le 9)
+
+	$i = 0
+	do
+		{
+			if ([string]$i -eq $NodeIP3) {$function3 = "ico$i"}
+			$i++
+		}  
+	while ($i -le 9)
+
+	[int]$off = 0
+	invoke-expression $function1
+
+	[int]$off = 5
+	invoke-expression $function2
+
+	[int]$off = 10
+	invoke-expression $function3
+
+	$ico = [System.Drawing.Icon]::FromHandle($bmp.GetHicon())
+	[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+	$balloon = New-Object System.Windows.Forms.NotifyIcon 
+
+	# Balloon icon type: None, Info, Warning or Error.
+	$balloon.BalloonTipTitle = "IT phone support: 888"
+	$balloon.BalloonTipText  = "IP Address: $IPAddress"
+	$balloon.BalloonTipIcon  = [System.Windows.Forms.ToolTipIcon]"Info"
+	$balloon.Icon = $ico
+	$balloon.Visible = $True
+	$balloon.ShowBalloonTip(30000)
+
+#	$balloon.Visible = $False
+
+
